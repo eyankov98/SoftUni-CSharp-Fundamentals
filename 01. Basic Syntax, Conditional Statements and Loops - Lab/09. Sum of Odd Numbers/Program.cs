550e0f9a608830949871c27be57cb0f6e65a53a2
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-29 19:48:57 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿int repeatTimes = int.Parse(Console.ReadLine()) * 2;
+
+int sum = 0;
+
+for (int i = 1; i <= repeatTimes; i++)
+{
+    if (i % 2 == 1)
+    {
+        Console.WriteLine(i);
+        sum += i;
+    }
+}
+
+Console.WriteLine($"Sum: {sum}");
