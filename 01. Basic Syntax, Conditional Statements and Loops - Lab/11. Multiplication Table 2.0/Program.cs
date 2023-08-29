b43914f209c6a1fc77709cf129ffc0a4b86b617a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-29 19:51:08 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,14 @@
+﻿int chosenNumber = int.Parse(Console.ReadLine());
+int multiplicationNumber = int.Parse(Console.ReadLine());
+
+if (multiplicationNumber > 10)
+{
+    Console.WriteLine($"{chosenNumber} X {multiplicationNumber} = {chosenNumber * multiplicationNumber}");
+}
+else
+{
+    for (int i = multiplicationNumber; i <= 10; i++)
+    {
+        Console.WriteLine($"{chosenNumber} X {i} = {chosenNumber * i}");
+    }
+}
