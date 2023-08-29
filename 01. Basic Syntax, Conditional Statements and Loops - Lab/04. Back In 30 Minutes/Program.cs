b9c3526b94150dbbc8167277c479d9c0ca704e37
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-29 19:44:59 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,20 @@
+﻿int currentHour = int.Parse(Console.ReadLine());
+int currentMinute = int.Parse(Console.ReadLine());
+
+if (currentMinute <= 29)
+{
+    Console.WriteLine($"{currentHour}:{currentMinute + 30}");
+}
+else
+{
+    if (currentHour == 23)
+    {
+        currentHour = 0;
+    }
+    else
+    {
+        currentHour++;
+    }
+
+    Console.WriteLine($"{currentHour}:{Math.Abs(currentMinute - 30):D2}");
+}
