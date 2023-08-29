b1e1782f6b09122e98e4ac63679e1cfd35ce19f0
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-29 19:47:19 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/06. Foreign Languages/Program.cs
@@ -0,0 +1,14 @@
+﻿string countryName = Console.ReadLine();
+
+if (countryName == "USA" || countryName == "England")
+{
+    Console.WriteLine("English");
+}
+else if (countryName == "Spain" || countryName == "Argentina" || countryName == "Mexico")
+{
+    Console.WriteLine("Spanish");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
