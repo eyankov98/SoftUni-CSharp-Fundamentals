1c85e8a5e90fc1bc72ec4408d4e35257dcd8fbac
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-29 19:42:43 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/01. Student Information/Program.cs
@@ -0,0 +1,5 @@
+﻿string studentName = Console.ReadLine();
+int studentAge = int.Parse(Console.ReadLine());
+double averageGrade = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Name: {studentName}, Age: {studentAge}, Grade: {averageGrade:f2}");
