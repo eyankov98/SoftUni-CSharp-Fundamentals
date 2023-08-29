5b88ec383ccb28f4f300b78e596fa57c4b10131c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-29 19:46:36 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/07. Theatre Promotion/Program.cs
@@ -0,0 +1,67 @@
+﻿string typeDay = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+
+int ticketPrice = 0;
+
+if (typeDay == "Weekday")
+{
+    if (age < 0 || age > 122)
+    {
+        Console.WriteLine("Error!");
+        return;
+    }
+    else if (age <= 18)
+    {
+        ticketPrice = 12;
+    }
+    else if (age <= 64)
+    {
+        ticketPrice = 18;
+    }
+    else if (age <= 122)
+    {
+        ticketPrice = 12;
+    }
+}
+else if (typeDay == "Weekend")
+{
+    if (age < 0 || age > 122)
+    {
+        Console.WriteLine("Error!");
+        return;
+    }
+    else if (age <= 18)
+    {
+        ticketPrice = 15;
+    }
+    else if (age <= 64)
+    {
+        ticketPrice = 20;
+    }
+    else if (age <= 122)
+    {
+        ticketPrice = 15;
+    }
+}
+else if (typeDay == "Holiday")
+{
+    if (age < 0 || age > 122)
+    {
+        Console.WriteLine("Error!");
+        return;
+    }
+    else if (age <= 18)
+    {
+        ticketPrice = 5;
+    }
+    else if (age <= 64)
+    {
+        ticketPrice = 12;
+    }
+    else if (age <= 122)
+    {
+        ticketPrice = 10;
+    }
+}
+
+Console.WriteLine($"{ticketPrice}$");
