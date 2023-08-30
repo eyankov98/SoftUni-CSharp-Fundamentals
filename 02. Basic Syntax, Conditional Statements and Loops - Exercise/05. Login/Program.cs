890ea8354cc6dce77346605084f239d992dedb8e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-30 21:02:59 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/05. Login/Program.cs
@@ -0,0 +1,31 @@
+﻿string password = Console.ReadLine();
+
+string passwordReverse = string.Empty;
+
+int countWrongPassword = 0;
+
+while (password != passwordReverse)
+{
+    passwordReverse = string.Empty;
+    string passwordRepeat = Console.ReadLine();
+
+    for (int i = passwordRepeat.Length - 1; i >= 0; i--)
+    {
+        passwordReverse = passwordReverse + passwordRepeat[i];
+    }
+
+    if (passwordReverse != password)
+    {
+        Console.WriteLine("Incorrect password. Try again.");
+
+        countWrongPassword++;
+
+        if (countWrongPassword == 3)
+        {
+            Console.WriteLine($"User {password} blocked!");
+            return;
+        }
+    }
+}
+
+Console.WriteLine($"User {password} logged in.");
