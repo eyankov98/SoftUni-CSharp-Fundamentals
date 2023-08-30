9cba57a9fdda94cf7ca3fdca390f85fa103a5634
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-30 21:04:35 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/08. Triangle of Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int rows = 1; rows <= n; rows++)
+{
+    for (int columns = 1; columns <= rows; columns++)
+    {
+        Console.Write($"{rows} ");
+    }
+    Console.WriteLine();
+}
