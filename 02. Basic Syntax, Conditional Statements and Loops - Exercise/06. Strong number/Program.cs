87bc57a635d0502a18b0079f023e41fcef2da98a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-30 21:04:09 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/06. Strong number/Program.cs
@@ -0,0 +1,27 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int strongNumber = number;
+int sum = 0;
+
+while (number != 0)
+{
+    int digit = number % 10;
+    number = number / 10;
+
+    int factoriel = 1;
+    for (int i = 1; i <= digit; i++)
+    {
+        factoriel = factoriel * i;
+    }
+
+    sum = sum + factoriel;
+}
+
+if (sum == strongNumber)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
