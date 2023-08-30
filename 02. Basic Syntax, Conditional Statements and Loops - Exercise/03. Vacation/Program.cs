57e3a0a82f35e0a29e70eda6e3808da86bd37f9f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-30 21:01:54 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/03. Vacation/Program.cs
@@ -0,0 +1,75 @@
+﻿int countOfPeople = int.Parse(Console.ReadLine());
+string typeOfGroup = Console.ReadLine();
+string dayOfWeek = Console.ReadLine();
+
+double priceForOnePerson = 0;
+double totalPrice = 0;
+
+if (typeOfGroup == "Students")
+{
+    if (dayOfWeek == "Friday")
+    {
+        priceForOnePerson = 8.45;
+    }
+    else if (dayOfWeek == "Saturday")
+    {
+        priceForOnePerson = 9.80;
+    }
+    else if (dayOfWeek == "Sunday")
+    {
+        priceForOnePerson = 10.46;
+    }
+
+    totalPrice = countOfPeople * priceForOnePerson;
+
+    if (countOfPeople >= 30)
+    {
+        totalPrice = totalPrice - totalPrice * 0.15;
+    }
+}
+else if (typeOfGroup == "Business")
+{
+    if (dayOfWeek == "Friday")
+    {
+        priceForOnePerson = 10.90;
+    }
+    else if (dayOfWeek == "Saturday")
+    {
+        priceForOnePerson = 15.60;
+    }
+    else if (dayOfWeek == "Sunday")
+    {
+        priceForOnePerson = 16;
+    }
+
+    totalPrice = countOfPeople * priceForOnePerson;
+
+    if (countOfPeople >= 100)
+    {
+        totalPrice = totalPrice - (10 * priceForOnePerson);
+    }
+}
+else if (typeOfGroup == "Regular")
+{
+    if (dayOfWeek == "Friday")
+    {
+        priceForOnePerson = 15;
+    }
+    else if (dayOfWeek == "Saturday")
+    {
+        priceForOnePerson = 20;
+    }
+    else if (dayOfWeek == "Sunday")
+    {
+        priceForOnePerson = 22.50;
+    }
+
+    totalPrice = countOfPeople * priceForOnePerson;
+
+    if (countOfPeople >= 10 && countOfPeople <= 20)
+    {
+        totalPrice = totalPrice - totalPrice * 0.05;
+    }
+}
+
+Console.WriteLine($"Total price: {totalPrice:f2}");
