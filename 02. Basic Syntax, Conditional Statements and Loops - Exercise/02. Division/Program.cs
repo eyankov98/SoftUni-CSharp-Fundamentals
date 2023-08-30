1347feff9f2610577f53b736e40f50d0dba49bf9
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-30 21:01:27 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/02. Division/Program.cs
@@ -0,0 +1,37 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int number = 0;
+
+if (n % 2 == 0)
+{
+    number = 2;
+}
+
+if (n % 3 == 0)
+{
+    number = 3;
+}
+
+if (n % 6 == 0)
+{
+    number = 6;
+}
+
+if (n % 7 == 0)
+{
+    number = 7;
+}
+
+if (n % 10 == 0)
+{
+    number = 10;
+}
+
+if (number == 0)
+{
+    Console.WriteLine("Not divisible");
+}
+else
+{
+    Console.WriteLine($"The number is divisible by {number}");
+}
