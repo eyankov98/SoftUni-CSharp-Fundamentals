acc25c37108b8c84d53e60fc3cbf1299c59eeee8
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-30 21:02:30 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/04. Print and sum/Program.cs
@@ -0,0 +1,14 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = n1; i <= n2; i++)
+{
+    Console.Write($"{i} ");
+
+    sum = sum + i;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Sum: {sum}");
