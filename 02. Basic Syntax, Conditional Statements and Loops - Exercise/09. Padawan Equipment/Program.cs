04f7b1aaad331c3d17f381b6ed1a4c7f2c971dd0
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-30 21:06:11 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/09. Padawan Equipment/Program.cs
@@ -0,0 +1,27 @@
+﻿double amounOfMoney = double.Parse(Console.ReadLine());
+int countOfStudents = int.Parse(Console.ReadLine());
+double priceOfLightsaber = double.Parse(Console.ReadLine());
+double priceOfRobe = double.Parse(Console.ReadLine());
+double priceOfBelt = double.Parse(Console.ReadLine());
+
+int countOfLightsaber = countOfStudents;
+int countOfRobe = countOfStudents;
+int countOfBelt = countOfStudents;
+
+double extraLightsaber = Math.Ceiling(countOfLightsaber * 0.10);
+int freeBelsts = countOfBelt / 6;
+
+countOfLightsaber = countOfLightsaber + (int)extraLightsaber;
+countOfBelt = countOfBelt - freeBelsts;
+
+double totalSum = (countOfLightsaber * priceOfLightsaber) + (countOfBelt * priceOfBelt) + (countOfRobe * priceOfRobe); ;
+
+if (totalSum > amounOfMoney)
+{
+    double result = totalSum - amounOfMoney;
+    Console.WriteLine($"John will need {result:f2}lv more.");
+}
+else
+{
+    Console.WriteLine($"The money is enough - it would cost {totalSum:f2}lv.");
+}
