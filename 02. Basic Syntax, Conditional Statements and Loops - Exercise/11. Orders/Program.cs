2f0d7bb150383014442f57daff5994adec4d5dd9
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-30 21:05:36 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/11. Orders/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+double totalPrice = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    double pricePerCapsule = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsulesCount = int.Parse(Console.ReadLine());
+
+    double price = pricePerCapsule * (days * capsulesCount);
+
+    totalPrice = totalPrice + price;
+
+    Console.WriteLine($"The price for the coffee is: ${price:f2}");
+}
+
+Console.WriteLine($"Total: ${totalPrice:f2}");
