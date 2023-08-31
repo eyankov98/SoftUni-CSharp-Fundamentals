d4a3380b5d4cc163f154456abe283dc78bfeed81
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-31 21:44:50 +0300

    Add files via upload

---
+++ 03. Basic Syntax, Conditional Statements and Loops - More Exercise/04. Reverse String/Program.cs
@@ -0,0 +1,10 @@
+﻿string username = Console.ReadLine();
+
+char[] cArray = username.ToCharArray();
+string reverseUsername = String.Empty;
+for (int i = cArray.Length - 1; i > -1; i--)
+{
+    reverseUsername += cArray[i];
+}
+
+Console.WriteLine(reverseUsername);
