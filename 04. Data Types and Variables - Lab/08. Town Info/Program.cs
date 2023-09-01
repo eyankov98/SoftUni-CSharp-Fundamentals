670c46e75ea31d301bc19edac34c04437cd34b84
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-01 22:55:02 +0300

    Add files via upload

---
+++ 04. Data Types and Variables - Lab/08. Town Info/Program.cs
@@ -0,0 +1,5 @@
+﻿string townName = Console.ReadLine();
+int population = int.Parse(Console.ReadLine());
+int area = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Town {0} has population of {1} and area {2} square km.", townName, population, area);
