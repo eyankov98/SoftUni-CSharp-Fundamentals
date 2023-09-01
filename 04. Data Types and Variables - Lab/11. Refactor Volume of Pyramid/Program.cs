4ff3a980ec14f520397146cb1442fcd36c432602
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-01 22:56:41 +0300

    Add files via upload

---
+++ 04. Data Types and Variables - Lab/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Length: "); double length = double.Parse(Console.ReadLine());
+
+Console.Write("Width: "); double width = double.Parse(Console.ReadLine());
+
+Console.Write("Height: "); double height = double.Parse(Console.ReadLine());
+
+double baseArea = length * width;
+
+double volume = ((baseArea * height) / 3.0);
+
+Console.WriteLine($"Pyramid Volume: {volume:f2}");
