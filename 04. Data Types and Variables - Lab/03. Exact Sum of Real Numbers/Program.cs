c1108f2ab02efd254db09e632c34d013493d9d39
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-01 22:51:38 +0300

    Add files via upload

---
+++ 04. Data Types and Variables - Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int numbersCount = int.Parse(Console.ReadLine());
+
+decimal numbersSum = 0;
+
+for (int i = 1; i <= numbersCount; i++)
+{
+    decimal inputNumber = decimal.Parse(Console.ReadLine());
+    numbersSum += inputNumber;
+}
+
+Console.WriteLine($"{numbersSum}");
