b5f93fbeeb4c3cc174d11c1a72d4c4304dc09282
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-01 22:56:06 +0300

    Add files via upload

---
+++ 04. Data Types and Variables - Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,10 @@
+﻿char inputSymbol = char.Parse(Console.ReadLine());
+
+if (inputSymbol >= 64 && inputSymbol <= 90)
+{
+    Console.WriteLine("upper-case");
+}
+else
+{
+    Console.WriteLine("lower-case");
+}
