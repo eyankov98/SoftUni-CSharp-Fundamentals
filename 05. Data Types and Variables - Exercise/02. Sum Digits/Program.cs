1c8b51479bcd7afec16fee79e7b88352e388d39a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:39:29 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    int digit = number % 10;
+
+    number = number / 10;
+
+    sum = sum + digit;
+}
+Console.WriteLine(sum);
