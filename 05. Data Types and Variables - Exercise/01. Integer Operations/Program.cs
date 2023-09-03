5c192cb499f2583d145b6f339c2b77388a1cfee2
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:38:59 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/01. Integer Operations/Program.cs
@@ -0,0 +1,6 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+int number4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine((number1 + number2) / number3 * number4);
