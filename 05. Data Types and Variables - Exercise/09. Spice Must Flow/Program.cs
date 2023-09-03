1e2eccb55175d769205631e3fbd613434ceecd30
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:45:10 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,23 @@
+﻿int startingSpice = int.Parse(Console.ReadLine());
+
+int days = 0;
+int totalSpice = 0;
+
+while (startingSpice >= 100)
+{
+    days++;
+    totalSpice = totalSpice + startingSpice;
+    totalSpice = totalSpice - 26;
+
+    startingSpice = startingSpice - 10;
+}
+
+Console.WriteLine(days);
+if (totalSpice < 26)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    Console.WriteLine(totalSpice - 26);
+}
