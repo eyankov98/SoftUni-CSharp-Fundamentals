05d8d371234d5bcdfec1064cf9bd8baf6653205a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:40:27 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    char charr = char.Parse(Console.ReadLine());
+
+    sum = sum + charr;
+}
+
+Console.WriteLine($"The sum equals: {sum}");
