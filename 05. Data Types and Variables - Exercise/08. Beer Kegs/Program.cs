09e9c4c47d87cc5e7dbef1b0d5fe8703c41e52d2
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:43:28 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+double volumeBiggestKeg = 0;
+string biggestKeg = string.Empty;
+
+for (int i = 1; i <= n; i++)
+{
+    string model = Console.ReadLine();
+    double radius = double.Parse(Console.ReadLine());
+    double height = double.Parse(Console.ReadLine());
+
+    double kegVolume = Math.PI * Math.Pow(radius, 2) * height;
+
+    if (kegVolume > volumeBiggestKeg)
+    {
+        volumeBiggestKeg = kegVolume;
+        biggestKeg = model;
+    }
+}
+
+Console.WriteLine(biggestKeg);
