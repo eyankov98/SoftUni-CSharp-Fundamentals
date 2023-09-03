6f544fcaa9afa24da5ec46cbea45177712052f68
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:41:56 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int quantitiesOfWater = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int liters = int.Parse(Console.ReadLine());
+
+    if (quantitiesOfWater + liters <= 255)
+    {
+        quantitiesOfWater = quantitiesOfWater + liters;
+    }
+    else
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+}
+
+Console.WriteLine(quantitiesOfWater);
