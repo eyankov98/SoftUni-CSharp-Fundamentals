5b9ba25f8c022164fb25bb9b4c22ed6c4c52932b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:42:19 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    char firstChar = (char)('a' + i);
+
+    for (int j = 0; j < n; j++)
+    {
+        char secondChar = (char)('a' + j);
+
+        for (int k = 0; k < n; k++)
+        {
+            char thirdChar = (char)('a' + k);
+
+            Console.WriteLine($"{firstChar}{(secondChar)}{thirdChar}");
+        }
+    }
+}
