168f931a9eff89b48a99c60bbe7016026a322a86
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:39:58 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberOfPeople = int.Parse(Console.ReadLine());
+int capacity = int.Parse(Console.ReadLine());
+
+int courses = numberOfPeople / capacity;
+
+if (numberOfPeople % capacity != 0)
+{
+    courses = courses + 1;
+}
+
+Console.WriteLine(courses);
