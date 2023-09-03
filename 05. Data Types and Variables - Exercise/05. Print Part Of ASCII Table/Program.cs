f84291b8a90391bb128ba76ad1200b6aae3a749c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:41:10 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,9 @@
+﻿int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+for (int i = start; i <= end; i++)
+{
+    char input = (char)i;
+
+    Console.Write($"{input} ");
+}
