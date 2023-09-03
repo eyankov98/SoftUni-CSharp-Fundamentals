acdad37fdf2d67549e6997c73614a4c948925760
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-03 18:45:51 +0300

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,20 @@
+﻿int N = int.Parse(Console.ReadLine());
+int M = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+
+int startingPower = N;
+int countOfTargets = 0;
+
+while (N >= M)
+{
+    N = N - M;
+    countOfTargets++;
+
+    if (N * 2 == startingPower && Y != 0)
+    {
+        N = N / Y;
+    }
+}
+
+Console.WriteLine(N);
+Console.WriteLine(countOfTargets);
