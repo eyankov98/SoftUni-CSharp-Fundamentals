31e2733debc6c795573f8c403bc0ae225c5f23b1
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-05 23:31:15 +0300

    Add files via upload

---
+++ 06. Data Types and Variables - More Exercise/05. Decrypting Message/Program.cs
@@ -0,0 +1,14 @@
+﻿int key = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+string message = string.Empty;
+for (int i = 0; i < n; i++)
+{
+    char input = char.Parse(Console.ReadLine());
+
+    int outputIntValue = input + key;
+    char outputChar = (char)outputIntValue;
+    message += outputChar;
+}
+
+Console.WriteLine(message);
