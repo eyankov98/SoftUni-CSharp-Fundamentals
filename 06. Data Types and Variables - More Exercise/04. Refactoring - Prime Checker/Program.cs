88c88158e9b4aff7fa76b682647c75e7ed731a51
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-05 23:29:56 +0300

    Add files via upload

---
+++ 06. Data Types and Variables - More Exercise/04. Refactoring - Prime Checker/Program.cs
@@ -0,0 +1,28 @@
+﻿int inputNumber = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= inputNumber; i++)
+{
+    bool isPrime = false;
+
+    for (int j = 2; j <= i; j++)
+    {
+        if (i == j)
+        {
+            isPrime = true;
+        }
+        else if (i % j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+    {
+        Console.WriteLine($"{i} -> true");
+    }
+    else
+    {
+        Console.WriteLine($"{i} -> false");
+    }
+}
