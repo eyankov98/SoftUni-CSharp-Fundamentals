790740299612471d7b797f418b83f03c07e66ba8
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-07 08:58:07 +0300

    Add files via upload

---
+++ 07. Arrays - Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int totalSum = 0;
+
+bool areSame = true;
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    totalSum = totalSum + firstArray[i];
+
+    if (firstArray[i] != secondArray[i])
+    {
+        areSame = false;
+
+        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+        break;
+    }
+}
+
+if (areSame)
+{
+    Console.WriteLine($"Arrays are identical. Sum: {totalSum}");
+}
