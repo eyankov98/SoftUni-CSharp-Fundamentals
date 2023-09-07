7abc2874daad5948005e48a8245d3ddbd737189a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-07 08:56:26 +0300

    Add files via upload

---
+++ 07. Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] input = Console.ReadLine().Split(' ').ToArray();
+
+for (int i = 0; i < input.Length / 2; i++)
+{
+    string firstElement = input[i];
+    string lastElement = input[input.Length - 1 - i];
+
+    input[i] = lastElement;
+    input[input.Length - 1 - i] = firstElement;
+}
+
+Console.WriteLine(string.Join(" ", input));
