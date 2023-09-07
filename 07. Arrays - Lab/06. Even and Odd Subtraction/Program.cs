be9d9971244476c5949d03e0a493574d981e438f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-07 08:57:46 +0300

    Add files via upload

---
+++ 07. Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int evenSum = 0;
+int oddSum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        evenSum = evenSum + numbers[i];
+    }
+    else
+    {
+        oddSum = oddSum + numbers[i];
+    }
+}
+
+Console.WriteLine(evenSum - oddSum);
