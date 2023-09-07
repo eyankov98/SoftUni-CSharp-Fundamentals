b62a25aa5e04b8e6dbb41b357567e636105dfb32
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-07 08:58:37 +0300

    Add files via upload

---
+++ 07. Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (int i = 0; i < numbers.Length - 1; i++)
+{
+    for (int j = 0; j < numbers.Length - 1 - i; j++)
+    {
+        numbers[j] = numbers[j] + numbers[j + 1];
+    }
+}
+
+Console.WriteLine(numbers[0]);
