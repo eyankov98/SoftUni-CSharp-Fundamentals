87bcda7be8e8fce066c050c9631c9809fb132209
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-09 20:13:56 +0300

    Add files via upload

---
+++ 08. Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+    int currentNumber = inputNumbers[i];
+
+    for (int j = i + 1; j < inputNumbers.Length; j++)
+    {
+        int sumOfNumbers = currentNumber + inputNumbers[j];
+
+        if (sumOfNumbers == number)
+        {
+            Console.WriteLine(currentNumber + " " + inputNumbers[j]);
+        }
+    }
+}
