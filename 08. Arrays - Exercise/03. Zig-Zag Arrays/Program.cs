27552e319a4a2b0d3903981a3dab716baf4c0bf1
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-09 20:09:28 +0300

    Add files via upload

---
+++ 08. Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] firstArray = new int[n];
+int[] secondArray = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int[] currentNumbers = Console.ReadLine()
+        .Split(" ")
+        .Select(int.Parse)
+        .ToArray();
+
+    if (i % 2 == 0)
+    {
+        firstArray[i] = currentNumbers[0];
+        secondArray[i] = currentNumbers[1];
+    }
+    else
+    {
+        secondArray[i] = currentNumbers[0];
+        firstArray[i] = currentNumbers[1];
+    }
+}
+
+Console.WriteLine(string.Join(" ", firstArray));
+Console.WriteLine(string.Join(" ", secondArray));
