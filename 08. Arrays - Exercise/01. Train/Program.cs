c9feb20c92f404a8757c7ed249808e7426868f26
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-09 20:08:30 +0300

    Add files via upload

---
+++ 08. Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] wagons = new int[n];
+
+int totalNumberOfPeople = 0;
+
+for (int i = 0; i < wagons.Length; i++)
+{
+    int numberOfPeople = int.Parse(Console.ReadLine());
+
+    wagons[i] = numberOfPeople;
+
+    totalNumberOfPeople = totalNumberOfPeople + numberOfPeople;
+}
+
+Console.WriteLine(string.Join(" ", wagons));
+Console.WriteLine(totalNumberOfPeople);
