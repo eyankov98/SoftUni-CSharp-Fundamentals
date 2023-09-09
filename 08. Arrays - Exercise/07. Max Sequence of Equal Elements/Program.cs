40f776bfaf94ec2ebdd222bd9dd938bd66b71e0d
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-09 20:12:15 +0300

    Add files via upload

---
+++ 08. Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int currentEqualElementsCount = 0;
+int equalElementsCount = 0;
+int currentIndexElement = 0;
+
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+    currentEqualElementsCount = 0;
+
+    for (int j = i; j < inputNumbers.Length; j++)
+    {
+        if (inputNumbers[i] == inputNumbers[j])
+        {
+            currentEqualElementsCount++;
+        }
+        else
+        {
+            break;
+        }
+
+        if (currentEqualElementsCount > equalElementsCount)
+        {
+            equalElementsCount = currentEqualElementsCount;
+            currentIndexElement = inputNumbers[i];
+        }
+    }
+}
+
+for (int i = 1; i <= equalElementsCount; i++)
+{
+    Console.Write(currentIndexElement + " ");
+}
