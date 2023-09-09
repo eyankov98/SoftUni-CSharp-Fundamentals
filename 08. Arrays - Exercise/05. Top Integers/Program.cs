6292dbba9000e9a485fdac74f1c2278b3fa95849
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-09 20:11:37 +0300

    Add files via upload

---
+++ 08. Arrays - Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+    int topInteger = inputNumbers[i];
+
+    bool isTopInteger = true;
+
+    for (int j = i + 1; j < inputNumbers.Length; j++)
+    {
+        if (topInteger <= inputNumbers[j])
+        {
+            isTopInteger = false;
+            break;
+        }
+    }
+    if (isTopInteger)
+    {
+        Console.Write(topInteger + " ");
+    }
+}
