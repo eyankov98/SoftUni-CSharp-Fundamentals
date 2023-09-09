fd65b61e65761b722ca9bcd699a3a2e24423b296
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-09 20:10:25 +0300

    Add files via upload

---
+++ 08. Arrays - Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int rotation = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotation; i++)
+{
+    int firstNumber = inputNumbers[0];
+
+    for (int j = 0; j < inputNumbers.Length - 1; j++)
+    {
+        inputNumbers[j] = inputNumbers[j + 1];
+    }
+
+    inputNumbers[inputNumbers.Length - 1] = firstNumber;
+}
+
+Console.WriteLine(string.Join(" ", inputNumbers));
