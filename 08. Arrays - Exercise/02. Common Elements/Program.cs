68408681a11bc3f3896b61c5eceed0ee0bf0ed5f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-09 20:09:46 +0300

    Add files via upload

---
+++ 08. Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] firstArray = Console.ReadLine()
+    .Split(" ")
+    .ToArray();
+
+string[] secondArray = Console.ReadLine()
+    .Split(" ")
+    .ToArray();
+
+for (int i = 0; i < secondArray.Length; i++)
+{
+    for (int j = 0; j < firstArray.Length; j++)
+    {
+        if (secondArray[i] == firstArray[j])
+        {
+            Console.Write(secondArray[i] + " ");
+        }
+    }
+}
