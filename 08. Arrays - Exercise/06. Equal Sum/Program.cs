e79ed2bdaa7b3bc16b9759b8244a0a741e5cfb5b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-09 20:12:40 +0300

    Add files via upload

---
+++ 08. Arrays - Exercise/06. Equal Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+if (inputNumbers.Length == 1)
+{
+    Console.WriteLine(0);
+    return;
+}
+
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+    int leftSum = 0;
+    int rightSum = 0;
+
+    for (int j = 0; j < i; j++)
+    {
+        leftSum = leftSum + inputNumbers[j];
+    }
+
+    for (int k = i + 1; k < inputNumbers.Length; k++)
+    {
+        rightSum = rightSum + inputNumbers[k];
+    }
+
+    if (leftSum == rightSum)
+    {
+        Console.WriteLine($"{i}");
+        return;
+    }
+}
+
+Console.WriteLine("no");
