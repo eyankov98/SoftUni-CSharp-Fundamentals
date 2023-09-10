1e2cb9dc83e7479494a3305e53d13d9269ef7670
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-10 11:48:30 +0300

    Add files via upload

---
+++ 09. Arrays - More Exercise/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿int input = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetFibonacci(input));
+
+static int GetFibonacci(int n)
+{
+    if (n == 2 || n == 1)
+    {
+        return 1;
+    }
+
+    int number = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+    return number;
+}
