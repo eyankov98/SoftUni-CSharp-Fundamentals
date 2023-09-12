c413489e3badf7536ebaf150c37280e6a457157a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-12 15:49:09 +0300

    Add files via upload

---
+++ 10. Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            double powerNumber = double.Parse(Console.ReadLine());
+
+            MathPower(baseNumber, powerNumber);
+        }
+
+        static void MathPower(double baseNum, double powerNum)
+        {
+            Console.WriteLine(Math.Pow(baseNum, powerNum));
+        }
+    }
+}
