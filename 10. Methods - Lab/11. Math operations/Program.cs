d925af4b6c27c9fc43b314c69c93f402b69c8639
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-12 15:51:48 +0300

    Add files via upload

---
+++ 10. Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            string calculator = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            if (calculator == "+")
+            {
+                Console.WriteLine(Addition(firstNumber, secondNumber));
+            }
+            else if (calculator == "*")
+            {
+                Console.WriteLine(Multiplication(firstNumber, secondNumber));
+            }
+            else if (calculator == "-")
+            {
+                Console.WriteLine(Subtraction(firstNumber, secondNumber));
+            }
+            else if (calculator == "/")
+            {
+                Console.WriteLine(Division(firstNumber, secondNumber));
+            }
+        }
+
+        static double Addition(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        static double Subtraction(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        static double Multiplication(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        static double Division(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
