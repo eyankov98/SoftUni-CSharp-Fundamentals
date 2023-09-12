ad110504158c74b1cd226787e8bd8503532d00d3
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-12 15:47:29 +0300

    Add files via upload

---
+++ 10. Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumberOne = int.Parse(Console.ReadLine());
+            int inputNumberTwo = int.Parse(Console.ReadLine());
+
+            AreaCalculation(inputNumberOne, inputNumberTwo);
+        }
+
+        static void AreaCalculation(int n1, int n2)
+        {
+            Console.WriteLine(n1 * n2);
+        }
+    }
+}
