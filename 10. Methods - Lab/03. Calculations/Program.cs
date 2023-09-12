0ad3f970b906de692ee900b43a0a174eecbeb524
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-12 15:46:24 +0300

    Add files via upload

---
+++ 10. Methods - Lab/03. Calculations/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string calculationType = Console.ReadLine();
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Calculations(firstNumber, calculationType, secondNumber);
+        }
+
+        static void Calculations(double firstNum, string action, double secondNum)
+        {
+            if (action == "add")
+            {
+                Console.WriteLine(firstNum + secondNum);
+            }
+            else if (action == "multiply")
+            {
+                Console.WriteLine(firstNum * secondNum);
+            }
+            else if (action == "subtract")
+            {
+                Console.WriteLine(firstNum - secondNum);
+            }
+            else if (action == "divide")
+            {
+                Console.WriteLine(firstNum / secondNum);
+            }
+        }
+    }
+}
