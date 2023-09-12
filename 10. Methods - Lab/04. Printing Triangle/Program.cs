6593372bfb26a9fa6344e58b5065d5004559f32c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-12 15:47:56 +0300

    Add files via upload

---
+++ 10. Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            TriangleOne(inputNumber);
+            TriangleTwo(inputNumber);
+        }
+
+        static void TriangleOne(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void TriangleTwo(int n)
+        {
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
