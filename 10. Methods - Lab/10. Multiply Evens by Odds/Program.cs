938eaae09f2db4c72e734e7d2577358271e85c5e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-12 15:50:49 +0300

    Add files via upload

---
+++ 10. Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNumberString = Console.ReadLine();
+            int inputNumber = int.Parse(inputNumberString);
+
+            int inputNumberAbs = (Math.Abs(inputNumber));
+            int inputNumberLength = inputNumberString.Length;
+
+            int evensSum = GetSumOfEvenDigits(inputNumberAbs, inputNumberLength);
+            int oddsSum = GetSumOfOddDigits(inputNumberAbs, inputNumberLength);
+            int evensMultipleOdds = GetMultipleOfEvenAndOdds(evensSum, oddsSum);
+
+            Console.WriteLine(evensMultipleOdds);
+        }
+
+        static int GetMultipleOfEvenAndOdds(int evensSum, int oddsSum)
+        {
+            return evensSum * oddsSum;
+        }
+
+        static int GetSumOfEvenDigits(int number, int length)
+        {
+            int evenDigitsSum = 0;
+
+            for (int i = length; i > 0; i--)
+            {
+                int lastDigit = number % 10;
+                number = (number - lastDigit) / 10;
+
+                if (lastDigit % 2 == 0)
+                {
+                    evenDigitsSum += lastDigit;
+                }
+            }
+
+            return evenDigitsSum;
+        }
+
+        static int GetSumOfOddDigits(int number, int length)
+        {
+            int oddDigitsSum = 0;
+
+            for (int i = length; i > 0; i--)
+            {
+                int lastDigit = number % 10;
+                number = (number - lastDigit) / 10;
+
+                if (lastDigit % 2 == 1)
+                {
+                    oddDigitsSum += lastDigit;
+                }
+            }
+
+            return oddDigitsSum;
+        }
+    }
+}
