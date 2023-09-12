a4ff29da0be75ebaeb559331d2ff3658acbe566b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-12 15:49:27 +0300

    Add files via upload

---
+++ 10. Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            int repeatCount = int.Parse(Console.ReadLine());
+
+            Repeat(inputString, repeatCount);
+        }
+
+        static void Repeat(string inputString, int repeatCount)
+        {
+            for (int i = 1; i <= repeatCount; i++)
+            {
+                Console.Write(inputString);
+            }
+        }
+    }
+}
