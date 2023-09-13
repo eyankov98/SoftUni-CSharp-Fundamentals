a1e2575d7c8545d1a5ca7c8ac2c9d1851e238e49
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-13 18:24:22 +0300

    Add files via upload

---
+++ 11. Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            MiddleCharacters(input);
+        }
+
+        static void MiddleCharacters(string input)
+        {
+            char[] chars = input.ToCharArray();
+
+            char firstMiddleChar;
+            char secondMiddleChar;
+
+            if (input.Length % 2 == 0)
+            {
+                firstMiddleChar = chars[input.Length / 2 - 1];
+                secondMiddleChar = chars[input.Length / 2];
+                Console.WriteLine($"{firstMiddleChar}{secondMiddleChar}");
+            }
+            else
+            {
+                firstMiddleChar = chars[input.Length / 2];
+                Console.WriteLine(firstMiddleChar);
+            }
+        }
+    }
+}
