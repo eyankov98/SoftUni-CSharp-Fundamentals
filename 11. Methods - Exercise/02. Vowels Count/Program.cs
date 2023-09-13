825d4b6e978638908ce4e8e01f8fba60f0a755b9
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-13 18:21:54 +0300

    Add files via upload

---
+++ 11. Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(VowelsCount(input));
+        }
+
+        static int VowelsCount(string input)
+        {
+            char[] inputArray = input.ToCharArray();
+            int vowelsCount = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (inputArray[i] == 'A' || inputArray[i] == 'E' || inputArray[i] == 'I'
+                    || inputArray[i] == 'O' || inputArray[i] == 'U' || inputArray[i] == 'a'
+                    || inputArray[i] == 'e' || inputArray[i] == 'i' || inputArray[i] == 'o'
+                    || inputArray[i] == 'u')
+                {
+                    vowelsCount++;
+                }
+            }
+
+            return vowelsCount;
+        }
+    }
+}
