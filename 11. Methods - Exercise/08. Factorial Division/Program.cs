2dc9bfe14558acd47cec0a9dec1272591e2413ac
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-13 18:25:53 +0300

    Add files via upload

---
+++ 11. Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            double firstNumFactorial = (double)(NumberFactorial(firstNumber));
+            double secondNumFactorial = (double)NumberFactorial(secondNumber);
+
+            Console.WriteLine($"{firstNumFactorial / secondNumFactorial:f2}");
+        }
+
+        static double NumberFactorial(int number)
+        {
+            double result = 1.00;
+
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
