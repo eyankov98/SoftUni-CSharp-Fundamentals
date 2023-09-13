364754f2d185a8436aa744f0bc50cace0155cc0f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-13 18:26:16 +0300

    Add files via upload

---
+++ 11. Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputInteger = int.Parse(Console.ReadLine());
+
+            Matrix(inputInteger);
+        }
+
+        static void Matrix(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= number; j++)
+                {
+                    Console.Write(number + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
