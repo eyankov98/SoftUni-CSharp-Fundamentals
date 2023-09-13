fb9e65319c07a5c23f72819ad45ffc098b4395d2
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-13 18:22:14 +0300

    Add files via upload

---
+++ 11. Methods - Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SmallestNumber(firstNumber, secondNumber, thirdNumber));
+        }
+
+        static int SmallestNumber(int n1, int n2, int n3)
+        {
+            int smallestNumber = int.MaxValue;
+
+            if (n1 < smallestNumber)
+            {
+                smallestNumber = n1;
+            }
+            if (n2 < smallestNumber)
+            {
+                smallestNumber = n2;
+            }
+            if (n3 < smallestNumber)
+            {
+                smallestNumber = n3;
+            }
+
+            return smallestNumber;
+        }
+    }
+}
