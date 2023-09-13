7e63a2f454786de1c9a735774b064928f209a7b6
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-13 18:22:52 +0300

    Add files via upload

---
+++ 11. Methods - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char firstCharCopy = firstChar;
+            char secondChar = char.Parse(Console.ReadLine());
+
+            if (firstChar > secondChar)
+            {
+                firstChar = secondChar;
+                secondChar = firstCharCopy;
+            }
+
+            int charsDistance = Math.Abs(firstChar - secondChar);
+
+            CharactersInRange(firstChar, secondChar, charsDistance);
+        }
+
+        static void CharactersInRange(char firstChar, char secondChar, int distance)
+        {
+            char[] charArray = new char[distance - 1];
+            int k = 0;
+
+            for (int i = firstChar + 1; i < secondChar; i++)
+            {
+                charArray[k] = (char)i;
+                k++;
+            }
+
+            for (int j = 0; j < distance - 1; j++)
+            {
+                if (j != distance - 2)
+                {
+                    Console.Write(charArray[j] + " ");
+                }
+                else
+                {
+                    Console.Write(charArray[j]);
+                }
+            }
+        }
+    }
+}
