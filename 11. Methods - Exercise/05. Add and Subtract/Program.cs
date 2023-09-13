ea5d01d0b4d930c1ce588616dfe2c5868e67f8bd
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-13 18:24:38 +0300

    Add files via upload

---
+++ 11. Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int d = Sum(a, b);
+            int result = Subtract(d, c);
+
+            Console.WriteLine(result);
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static int Subtract(int d, int c)
+        {
+            return d - c;
+        }
+    }
+}
