410ab91818e6f847148766916b8aa0dc9613fb84
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-14 21:55:29 +0300

    Add files via upload

---
+++ 12. Methods - More Exercise/01. Data Types/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01._Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string inputType = Console.ReadLine();
+
+                if (inputType == "int")
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    Integers(input);
+                }
+                else if (inputType == "real")
+                {
+                    double input = double.Parse(Console.ReadLine());
+                    RealNums(input);
+                }
+                else if (inputType == "string")
+                {
+                    string input = Console.ReadLine();
+                    Strings(input);
+                }
+            }
+
+            static void Integers(int input)
+            {
+                Console.WriteLine(input * 2);
+            }
+            static void RealNums(double input)
+            {
+                Console.WriteLine($"{(input * 1.5):f2}");
+            }
+            static void Strings(string input)
+            {
+                Console.WriteLine($"${input}$");
+            }
+        }
+    }
+}
