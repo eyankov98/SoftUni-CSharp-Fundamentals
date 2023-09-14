34ea9b1152467be36fe245a3b306e43b9312b343
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-14 21:57:37 +0300

    Add files via upload

---
+++ 12. Methods - More Exercise/05. Multiplication Sign/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _05._Multiplication_Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int numOne = int.Parse(Console.ReadLine());
+                int numTwo = int.Parse(Console.ReadLine());
+                int numThree = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(PositiveOrNegative(numOne, numTwo, numThree));
+            }
+
+            static string PositiveOrNegative(int numOne, int numTwo, int numThree)
+            {
+                int negativeNumsCnt = 0;
+
+                if (numThree == 0 || numTwo == 0 || numThree == 0)
+                {
+                    return "zero";
+                }
+
+                if (numOne < 0)
+                {
+                    negativeNumsCnt++;
+                }
+
+                if (numTwo < 0)
+                {
+                    negativeNumsCnt++;
+                }
+
+                if (numThree < 0)
+                {
+                    negativeNumsCnt++;
+                }
+
+                if (negativeNumsCnt % 2 == 0)
+                {
+                    return "positive";
+                }
+                else
+                {
+                    return "negative";
+                }
+            }
+        }
+    }
+}
