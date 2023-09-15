cccbf0fa33473fea6a7fdb3a990ce782918b8a10
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-15 23:21:16 +0300

    Add files via upload

---
+++ 13. Lists - Lab/07. List Manipulation Advanced/Program.cs
@@ -0,0 +1,143 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+string command = string.Empty;
+bool madeChanges = false;
+
+while ((command = Console.ReadLine()) != "end")
+{
+    if (command.Contains("Add") == true)
+    {
+        madeChanges = true;
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+
+        numbers.Add(value);
+    }
+    else if (command.Contains("RemoveAt") == true)
+    {
+        madeChanges = true;
+        string[] commandArray = command.Split();
+        int index = int.Parse(commandArray[1]);
+
+        numbers.RemoveAt(index);
+    }
+    else if (command.Contains("Remove") == true)
+    {
+        madeChanges = true;
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+
+        numbers.Remove(value);
+    }
+    else if (command.Contains("Insert") == true)
+    {
+        madeChanges = true;
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+        int index = int.Parse(commandArray[2]);
+
+        numbers.Insert(index, value);
+    }
+    else if (command.Contains("Contains") == true)
+    {
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+
+        if (numbers.Contains(value))
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No such number");
+        }
+    }
+    else if (command == "PrintEven")
+    {
+        List<int> evenNumbersList = new List<int>();
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                evenNumbersList.Add(numbers[i]);
+            }
+        }
+        Console.WriteLine(String.Join(" ", evenNumbersList));
+    }
+    else if (command == "PrintOdd")
+    {
+        List<int> oddNumbersList = new List<int>();
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] % 2 == 1)
+            {
+                oddNumbersList.Add(numbers[i]);
+            }
+        }
+        Console.WriteLine(String.Join(" ", oddNumbersList));
+    }
+    else if (command == "GetSum")
+    {
+        Console.WriteLine(numbers.Sum());
+    }
+    else if (command.Contains("Filter") == true)
+    {
+        string[] commandArray = command.Split();
+        string condition = commandArray[1];
+        int value = int.Parse(commandArray[2]);
+
+        List<int> conditionNumbersList = new List<int>();
+
+        if (condition == "<")
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < value)
+                {
+                    conditionNumbersList.Add(numbers[i]);
+                }
+            }
+        }
+        else if (condition == ">")
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > value)
+                {
+                    conditionNumbersList.Add(numbers[i]);
+                }
+            }
+        }
+        else if (condition == ">=")
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] >= value)
+                {
+                    conditionNumbersList.Add(numbers[i]);
+                }
+            }
+        }
+        else if (condition == "<=")
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] <= value)
+                {
+                    conditionNumbersList.Add(numbers[i]);
+                }
+            }
+        }
+        Console.WriteLine(String.Join(" ", conditionNumbersList));
+    }
+}
+
+if (madeChanges == true)
+{
+    Console.WriteLine(String.Join(" ", numbers));
+}
