42b0ae21ae42c71c30333e0d1abb4a7c984c7bb9
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-15 23:19:52 +0300

    Add files via upload

---
+++ 13. Lists - Lab/04. List of Products/Program.cs
@@ -0,0 +1,15 @@
+﻿int productsCount = int.Parse(Console.ReadLine());
+
+List<string> productsList = new List<string>();
+
+for (int i = 0; i < productsCount; i++)
+{
+    productsList.Add(Console.ReadLine());
+}
+
+productsList.Sort();
+
+for (int j = 1; j <= productsCount; j++)
+{
+    Console.WriteLine($"{j}.{productsList[j - 1]}");
+}
