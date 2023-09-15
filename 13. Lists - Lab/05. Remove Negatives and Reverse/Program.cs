358a6fa7e80c6cf1771c7dd7d8ab01e1fc4693ff
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-15 23:20:09 +0300

    Add files via upload

---
+++ 13. Lists - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+int numbersCnt = numbers.Count;
+
+int negativeNumsCnt = 0;
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (numbers[i] < 0)
+    {
+        negativeNumsCnt += 1;
+        numbers.RemoveAt(i);
+        i -= 1;
+    }
+}
+
+if (negativeNumsCnt == numbersCnt)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    numbers.Reverse();
+    Console.WriteLine(String.Join(" ", numbers));
+}
