f8d2cbb3ad4ea1814c86084b2a33a483ea5a0da6
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-15 23:18:31 +0300

    Add files via upload

---
+++ 13. Lists - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (i != numbers.Count - 1)
+    {
+        numbers[i] += numbers[numbers.Count - 1];
+        numbers.RemoveAt(numbers.Count - 1);
+    }
+}
+
+Console.WriteLine(String.Join(" ", numbers));
