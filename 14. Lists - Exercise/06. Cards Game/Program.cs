9a51f4c04f05fca7ded3cf1af054d09339fa1ea5
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-16 22:37:25 +0300

    Add files via upload

---
+++ 14. Lists - Exercise/06. Cards Game/Program.cs
@@ -0,0 +1,52 @@
+﻿List<int> firstPlayerCards = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+List<int> secondPlayerCards = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+while (firstPlayerCards.Count > 0 && secondPlayerCards.Count > 0)
+{
+    if (firstPlayerCards[0] > secondPlayerCards[0])
+    {
+        firstPlayerCards.Add(firstPlayerCards[0]);
+        firstPlayerCards.Add(secondPlayerCards[0]);
+
+        firstPlayerCards.RemoveAt(0);
+        secondPlayerCards.RemoveAt(0);
+    }
+    else if (firstPlayerCards[0] < secondPlayerCards[0])
+    {
+        secondPlayerCards.Add(secondPlayerCards[0]);
+        secondPlayerCards.Add(firstPlayerCards[0]);
+
+        secondPlayerCards.RemoveAt(0);
+        firstPlayerCards.RemoveAt(0);
+    }
+    else
+    {
+        firstPlayerCards.RemoveAt(0);
+        secondPlayerCards.RemoveAt(0);
+    }
+}
+
+int winnerCardsSum = 0;
+if (firstPlayerCards.Count == 0)
+{
+    for (int j = 0; j < secondPlayerCards.Count; j++)
+    {
+        winnerCardsSum += secondPlayerCards[j];
+    }
+    Console.WriteLine($"Second player wins! Sum: {winnerCardsSum}");
+}
+else if (secondPlayerCards.Count == 0)
+{
+    for (int k = 0; k < firstPlayerCards.Count; k++)
+    {
+        winnerCardsSum += firstPlayerCards[k];
+    }
+    Console.WriteLine($"First player wins! Sum: {winnerCardsSum}");
+}
