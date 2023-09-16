04eaa4998feb6d57ce3642e60767b1f3aedabe9a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-16 22:36:52 +0300

    Add files via upload

---
+++ 14. Lists - Exercise/05. Bomb Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿List<int> numList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+int[] bombNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int originalCount = numList.Count;
+
+for (int i = 0; i < numList.Count; i++)
+{
+    if (numList[i] == bombNum[0])
+    {
+        int originalJ = i - bombNum[1];
+
+        if (originalJ < 0)
+        {
+            originalJ = 0;
+        }
+
+        for (int j = originalJ; j <= (bombNum[1] * 2) + originalJ; j++)
+        {
+            if (originalJ >= numList.Count)
+            {
+                break;
+            }
+            else
+            {
+                numList.RemoveAt(originalJ);
+            }
+
+        }
+
+        i = 0;
+    }
+}
+
+int sum = 0;
+
+foreach (int item in numList)
+{
+    sum += item;
+}
+
+Console.WriteLine(sum);
