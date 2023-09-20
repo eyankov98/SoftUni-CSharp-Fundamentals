c079882401c545d411b8fa363d861e232d7e479d
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-20 17:14:29 +0300

    Add files via upload

---
+++ 16. Programming Fundamentals Mid Exam/01. SoftUni Reception/Program.cs
@@ -0,0 +1,22 @@
+﻿int firstEmployeeEfficiency = int.Parse(Console.ReadLine());
+int secondEmployeeEfficiency = int.Parse(Console.ReadLine());
+int thirdEmployeeEfficiency = int.Parse(Console.ReadLine());
+
+int studentsCount = int.Parse(Console.ReadLine());
+
+int hoursCount = 0;
+
+int answersStudentsForHour = firstEmployeeEfficiency + secondEmployeeEfficiency + thirdEmployeeEfficiency;
+
+while (studentsCount > 0)
+{
+    hoursCount++;
+
+    studentsCount = studentsCount - answersStudentsForHour;
+
+    if (hoursCount % 4 == 0)
+    {
+        hoursCount++;
+    }
+}
+Console.WriteLine($"Time needed: {hoursCount}h.");
