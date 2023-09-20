3347d0edaf02890b2d01117584334234dba7729b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-20 17:32:43 +0300

    Add files via upload

---
+++ 17. Programming Fundamentals Mid Exam/01. Guinea Pig/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01._Guinea_Pig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal foodInKilograms = decimal.Parse(Console.ReadLine());
+            decimal hayInKilograms = decimal.Parse(Console.ReadLine());
+            decimal coverInKilograms = decimal.Parse(Console.ReadLine());
+            decimal pigWeightInKilograms = decimal.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 30; i++)
+            {
+                foodInKilograms -= 0.3m;
+
+                if (i % 2 == 0)
+                {
+                    hayInKilograms -= foodInKilograms * 0.05m;
+                }
+                if (i % 3 == 0)
+                {
+                    coverInKilograms -= pigWeightInKilograms / 3.0m;
+                }
+
+                if (foodInKilograms <= 0 || hayInKilograms <= 0 || coverInKilograms <= 0)
+                {
+                    break;
+                }
+            }
+
+            if (foodInKilograms <= 0 || hayInKilograms <= 0 || coverInKilograms <= 0)
+            {
+                Console.WriteLine("Merry must go to the pet store!");
+            }
+            else
+            {
+                Console.WriteLine($"Everything is fine! Puppy is happy! Food: {foodInKilograms:f2}, Hay: {hayInKilograms:f2}, Cover: {coverInKilograms:f2}.");
+            }
+        }
+    }
+}
