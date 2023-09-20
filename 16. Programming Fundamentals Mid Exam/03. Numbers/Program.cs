b1627c9f62dfa5ec4f7ead4b0dbdf75cc6ec8e6d
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-20 17:26:54 +0300

    Add files via upload

---
+++ 16. Programming Fundamentals Mid Exam/03. Numbers/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace _03._Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int[] inputArray = input.Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int numbersSum = 0;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                numbersSum += inputArray[i];
+            }
+
+            double averageNumber = (double)numbersSum / (double)inputArray.Length;
+
+            int[] greaterNumbers = new int[inputArray.Length];
+            int greaterNumbersCounter = 0;
+
+            int k = 0;
+
+            for (int j = 0; j < inputArray.Length; j++)
+            {
+                if (inputArray[j] > averageNumber)
+                {
+                    greaterNumbers[k] = inputArray[j];
+                    greaterNumbersCounter++;
+                    k++;
+                }
+
+            }
+
+            if (averageNumber >= 0)
+            {
+                int[] outputArray = new int[greaterNumbersCounter];
+                for (int i = 0; i < greaterNumbersCounter; i++)
+                {
+                    outputArray[i] = greaterNumbers[i];
+                }
+
+                Array.Sort(outputArray);
+                Array.Reverse(outputArray);
+
+                if (greaterNumbersCounter >= 5)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.Write($"{outputArray[i]} ");
+                    }
+                }
+                else if (greaterNumbersCounter == 0)
+                {
+                    Console.WriteLine("No");
+                }
+                else
+                {
+                    for (int i = 0; i < greaterNumbersCounter; i++)
+                    {
+                        Console.Write($"{outputArray[i]} ");
+                    }
+                }
+            }
+            else
+            {
+                int[] outputArray = new int[greaterNumbersCounter];
+
+                for (int i = 0; i < greaterNumbersCounter; i++)
+                {
+                    outputArray[i] = greaterNumbers[i];
+                }
+                Array.Sort(outputArray);
+
+                if (greaterNumbersCounter >= 5)
+                {
+                    for (int i = 5; i > 0; i--)
+                    {
+                        greaterNumbersCounter--;
+                        Console.Write($"{outputArray[greaterNumbersCounter]} ");
+                    }
+                }
+                else if (greaterNumbersCounter == 0)
+                {
+                    Console.WriteLine("No");
+                }
+                else
+                {
+                    for (int i = greaterNumbersCounter - 1; i >= 0; i--)
+                    {
+                        Console.Write($"{outputArray[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}
