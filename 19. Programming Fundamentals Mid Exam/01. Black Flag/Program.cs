65e8c4e9c791221cb3cf306cc80fb64404a9165f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-21 17:44:55 +0300

    Add files via upload

---
+++ 19. Programming Fundamentals Mid Exam/01. Black Flag/Program.cs
@@ -0,0 +1,28 @@
+﻿int days = int.Parse(Console.ReadLine());
+int dailyPlunder = int.Parse(Console.ReadLine());
+double expectedPlunder = double.Parse(Console.ReadLine());
+
+double allPlunder = 0;
+for (int i = 1; i <= days; i++)
+{
+    allPlunder += dailyPlunder;
+
+    if (i % 3 == 0)
+    {
+        allPlunder += (double)dailyPlunder / 2;
+    }
+    if (i % 5 == 0)
+    {
+        allPlunder *= 0.7;
+    }
+}
+
+if (allPlunder >= expectedPlunder)
+{
+    Console.WriteLine($"Ahoy! {allPlunder:f2} plunder gained.");
+}
+else
+{
+    double collectedPercentage = (allPlunder / expectedPlunder) * 100;
+    Console.WriteLine($"Collected only {collectedPercentage:f2}% of the plunder.");
+}
