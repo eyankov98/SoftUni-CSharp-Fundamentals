4fc10ca07a079bb2c37ddb9fe2371fcb17e92dc4
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-21 17:42:11 +0300

    Add files via upload

---
+++ 18. Programming Fundamentals Mid Exam/01. Bonus Scoring System/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01._Bonus_Scoring_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsCount = int.Parse(Console.ReadLine());
+            int lecturesCount = int.Parse(Console.ReadLine());
+            int additionalBonus = int.Parse(Console.ReadLine());
+
+            double maxBonus = double.MinValue;
+            int maxBonusStudentAttendances = 0;
+
+            if (studentsCount == 0 || lecturesCount == 0)
+            {
+                Console.WriteLine($"Max Bonus: 0.");
+                Console.WriteLine($"The student has attended 0 lectures.");
+                return;
+            }
+
+            for (int i = 1; i <= studentsCount; i++)
+            {
+                int studentAttendances = int.Parse(Console.ReadLine());
+
+                double totalBonus = ((double)studentAttendances / lecturesCount) *
+                    (5 + additionalBonus);
+
+                if (maxBonus < totalBonus)
+                {
+                    maxBonus = totalBonus;
+                    maxBonusStudentAttendances = studentAttendances;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(maxBonus)}.");
+            Console.WriteLine($"The student has attended {maxBonusStudentAttendances} lectures.");
+        }
+    }
+}
