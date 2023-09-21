822c96029defbcc9ea8bca8dc9652f180a2bdc34
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-21 17:42:34 +0300

    Add files via upload

---
+++ 18. Programming Fundamentals Mid Exam/02. MuOnline/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _02._MuOnline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dungeonRooms = Console.ReadLine()
+                .Split("|")
+                .ToList();
+
+            int initialHealth = 100;
+            int initialBitcoins = 0;
+            int bestRoom = 0;
+
+            for (int i = 0; i < dungeonRooms.Count; i++)
+            {
+                bestRoom = i;
+
+                string[] currentRoom = dungeonRooms[i].Split();
+                int number = Convert.ToInt32(currentRoom[1].ToString());
+
+                if (currentRoom[0] == "potion")
+                {
+                    if (initialHealth + number <= 100)
+                    {
+                        initialHealth += number;
+                        Console.WriteLine($"You healed for {number} hp.");
+                        Console.WriteLine($"Current health: {initialHealth} hp.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You healed for {100 - initialHealth} hp.");
+                        initialHealth = 100;
+                        Console.WriteLine($"Current health: {initialHealth} hp.");
+                    }
+                }
+                else if (currentRoom[0] == "chest")
+                {
+                    Console.WriteLine($"You found {number} bitcoins.");
+                    initialBitcoins += number;
+                }
+                else
+                {
+                    initialHealth -= number;
+
+                    if (initialHealth > 0)
+                    {
+                        Console.WriteLine($"You slayed {currentRoom[0]}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {currentRoom[0]}.");
+                        bestRoom++;
+                        Console.WriteLine($"Best room: {bestRoom}");
+                        break;
+                    }
+                }
+            }
+
+            if (initialHealth > 0)
+            {
+                Console.WriteLine($"You've made it!");
+                Console.WriteLine($"Bitcoins: {initialBitcoins}");
+                Console.WriteLine($"Health: {initialHealth}");
+            }
+        }
+    }
+}
