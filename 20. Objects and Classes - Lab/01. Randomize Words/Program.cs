45a579f6e97f7bab561bed0bd20229547a3d1a76
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-22 14:54:16 +0300

    Add files via upload

---
+++ 20. Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] words = Console.ReadLine()
+    .Split(" ")
+    .ToArray();
+
+for (int i = 0; i < words.Length; i++)
+{
+    Random random = new Random();
+    int randomindex = random.Next(0, words.Length);
+
+    string currentWord = words[i];
+    string wordToSwap = words[randomindex];
+
+    words[i] = wordToSwap;
+    words[randomindex] = currentWord;
+}
+Console.WriteLine(string.Join(Environment.NewLine, words));
