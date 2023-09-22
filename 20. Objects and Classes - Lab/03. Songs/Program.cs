75326b9ab4e97102ac2008a1762e414364876685
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-22 14:54:58 +0300

    Add files via upload

---
+++ 20. Objects and Classes - Lab/03. Songs/Program.cs
@@ -0,0 +1,55 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<Song> songs = new List<Song>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine()
+        .Split("_")
+        .ToArray();
+
+    string typeList = input[0];
+    string songName = input[1];
+    string duration = input[2];
+
+    Song song = new Song
+    {
+        TypeList = typeList,
+        Name = songName,
+        Time = duration
+    };
+
+    songs.Add(song);
+}
+
+string searchForTypeList = Console.ReadLine();
+
+if (searchForTypeList == "all")
+{
+    // var searchSongs = songs.Select(x => x.Name);
+    // Console.WriteLine(string.Join(" ", searchSongs));
+    foreach (var song in songs)
+    {
+        Console.WriteLine(song.Name);
+    }
+}
+else
+{
+    // var searchSongs = songs.where(x => x.TypeList == searchForTypeList).Select(x => x.Name);
+    // Console.WriteLine(string.Join(" ", searchSongs));
+    foreach (var song in songs)
+    {
+        if (song.TypeList == searchForTypeList)
+        {
+            Console.WriteLine(song.Name);
+        }
+    }
+}
+public class Song
+{
+    public string TypeList { get; set; }
+
+    public string Name { get; set; }
+
+    public string Time { get; set; }
+}
