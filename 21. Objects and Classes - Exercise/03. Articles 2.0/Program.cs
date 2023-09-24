0d08761941f8ec9eb92d2ac1e59e4a04fd38bf81
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-24 09:46:09 +0300

    Add files via upload

---
+++ 21. Objects and Classes - Exercise/03. Articles 2.0/Program.cs
@@ -0,0 +1,41 @@
+﻿List<Article> articleList = new List<Article>();
+int commandsCount = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= commandsCount; i++)
+{
+    string[] inputs = Console.ReadLine().Split(", ");
+
+    string title = inputs[0];
+    string content = inputs[1];
+    string author = inputs[2];
+
+    Article article = new Article(title, content, author);
+
+    articleList.Add(article);
+}
+
+string command = Console.ReadLine();
+
+foreach (var currentArticle in articleList)
+{
+    Console.WriteLine(currentArticle);
+}
+
+class Article
+{
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Content}: {this.Author}";
+    }
+}
