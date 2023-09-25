ae39620786d0b58746c7c36861cf93a9e2b101dd
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-25 10:38:52 +0300

    Add files via upload

---
+++ 22. Objects and Classes - More Exercise/02. Oldest Family Member/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _02._Oldest_Family_Member
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfMembers = int.Parse(Console.ReadLine());
+
+            Family thisFamily = new Family();
+
+            for (int i = 0; i < numberOfMembers; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                thisFamily.AddMember(input[0], int.Parse(input[1]));
+            }
+
+            Person oldestMember = thisFamily.GetOldestMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public Person()
+            {
+                this.Name = "No Name";
+                this.Age = 0;
+            }
+            public Person(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+        }
+
+        public class Family
+        {
+            List<Person> People { get; set; }
+
+            public void AddMember(string name, int age)
+            {
+                Person newMember = new Person(name, age);
+                People.Add(newMember);
+            }
+
+            public Person GetOldestMember()
+            {
+                return this.People.OrderByDescending(x => x.Age).FirstOrDefault();
+            }
+
+            public Family()
+            {
+                this.People = new List<Person>();
+            }
+        }  
+    }
+}
