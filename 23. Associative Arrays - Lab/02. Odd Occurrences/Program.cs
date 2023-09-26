69d0e5ae36184b796e9fd3c210924ff6697f669f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-26 19:16:28 +0300

    Add files via upload

---
+++ 23. Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] words = Console.ReadLine()
+    .Split(" ")
+    .ToArray();
+
+Dictionary<string, int> allWords = new Dictionary<string, int>();
+
+foreach (string word in words)
+{
+    string wordsToLower = word.ToLower();
+
+    if (!allWords.ContainsKey(wordsToLower))
+    {
+        allWords.Add(wordsToLower, 0);
+    }
+
+    allWords[wordsToLower]++;
+}
+
+var result = allWords
+    .Where(x => x.Value % 2 != 0)
+    .Select(x => x.Key)
+    .ToList();
+
+Console.WriteLine(string.Join(" ", result));
