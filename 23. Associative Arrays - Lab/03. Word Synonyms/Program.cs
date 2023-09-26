1842b6b0e4b7d9216aa6b0491bf0f40f7a7687d2
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-26 19:17:27 +0300

    Add files via upload

---
+++ 23. Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < n; i++)
+{
+    string key = Console.ReadLine();
+    string value = Console.ReadLine();
+
+    if (!words.ContainsKey(key))
+    {
+        List<string> newWords = new List<string>();
+
+        newWords.Add(value);
+
+        words.Add(key, newWords);
+    }
+    else
+    {
+        words[key].Add(value);
+    }
+}
+
+foreach (var word in words)
+{
+    Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+}
