c7369c2ca542356d70d39a696f8a488a416796c5
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-27 19:16:39 +0300

    Add files via upload

---
+++ 24. Associative Arrays - Exercise/07. Company Users/Program.cs
@@ -0,0 +1,36 @@
+﻿string info = string.Empty;
+
+Dictionary<string, List<string>> employees = new Dictionary<string, List<string>>();
+
+while ((info = Console.ReadLine()) != "End")
+{
+    string[] infoArray = info.Split(" -> ");
+    string company = infoArray[0];
+    string id = infoArray[1];
+
+    if (!employees.ContainsKey(company))
+    {
+        List<string> idsList = new List<string>
+        {
+            id
+        };
+        employees.Add(company, idsList);
+    }
+    else
+    {
+        if (!employees[company].Contains(id))
+        {
+            employees[company].Add(id);
+        }
+    }
+}
+
+foreach (var employee in employees)
+{
+    Console.WriteLine(employee.Key);
+
+    for (int i = 0; i < employee.Value.Count; i++)
+    {
+        Console.WriteLine($"-- {employee.Value[i]}");
+    }
+}
