69d0beecd8149d89475fe761e9c403ffba5fc3e1
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-27 19:12:49 +0300

    Add files via upload

---
+++ 24. Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,22 @@
+﻿string info = string.Empty;
+
+Dictionary<string, int> resources = new Dictionary<string, int>();
+
+while ((info = Console.ReadLine()) != "stop")
+{
+    int quantity = int.Parse(Console.ReadLine());
+
+    if (!resources.ContainsKey(info))
+    {
+        resources.Add(info, quantity);
+    }
+    else
+    {
+        resources[info] += quantity;
+    }
+}
+
+foreach (var resource in resources)
+{
+    Console.WriteLine($"{resource.Key} -> {resource.Value}");
+}
