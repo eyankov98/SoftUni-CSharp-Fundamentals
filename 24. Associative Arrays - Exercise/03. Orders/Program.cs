ff894fa26eae59a7b57be928102ce9992d4a1c4f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-27 19:13:26 +0300

    Add files via upload

---
+++ 24. Associative Arrays - Exercise/03. Orders/Program.cs
@@ -0,0 +1,36 @@
+﻿string info = string.Empty;
+
+Dictionary<string, decimal> productsPrices = new Dictionary<string, decimal>();
+Dictionary<string, decimal> productsQuantities = new Dictionary<string, decimal>();
+
+while ((info = Console.ReadLine()) != "buy")
+{
+    string[] infoArray = info.Split();
+
+    string productName = infoArray[0];
+    decimal productPrice = decimal.Parse(infoArray[1]);
+    int productCount = int.Parse(infoArray[2]);
+
+    if (!productsQuantities.ContainsKey(productName))
+    {
+        productsQuantities.Add(productName, productCount);
+    }
+    else
+    {
+        productsQuantities[productName] += productCount;
+    }
+
+    if (!productsPrices.ContainsKey(productName))
+    {
+        productsPrices.Add(productName, productPrice * productCount);
+    }
+    else
+    {
+        productsPrices[productName] = productPrice * productsQuantities[productName];
+    }
+}
+
+foreach (var product in productsPrices)
+{
+    Console.WriteLine($"{product.Key} -> {product.Value:f2}");
+}
