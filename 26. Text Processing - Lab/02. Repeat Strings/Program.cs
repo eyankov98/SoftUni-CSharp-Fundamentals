d56d845edcd15c398335d47820a5d561a3738511
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-29 14:34:04 +0300

    Add files via upload

---
+++ 26. Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] words = Console.ReadLine()
+    .Split(" ")
+    .ToArray();
+
+string result = string.Empty;
+
+foreach (string word in words)
+{
+    for (int i = 0; i < word.Length; i++)
+    {
+        result = result + word;
+    }
+}
+
+Console.WriteLine(result);
