774dfb47d6e21de69b9aaa3d5d2b400663c355e5
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-29 14:33:25 +0300

    Add files via upload

---
+++ 26. Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+
+while (input != "end")
+{
+    char[] reversedWord = input.Reverse().ToArray();
+
+    Console.WriteLine($"{input} = {string.Join("", reversedWord)}");
+
+    input = Console.ReadLine();
+}
