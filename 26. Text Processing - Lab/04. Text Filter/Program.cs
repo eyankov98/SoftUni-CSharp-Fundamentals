77ef4b3420d3c320f8dfef54b7fe74e074c7647a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-29 14:35:38 +0300

    Add files via upload

---
+++ 26. Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] bannedWords = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+string text = Console.ReadLine();
+
+foreach (string banedWord in bannedWords)
+{
+    text = text.Replace(banedWord, new string('*', banedWord.Length));
+}
+
+Console.WriteLine(text);
