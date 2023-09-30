fac63ab88dfb9459381a16333dabb654fbf43c3e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-30 21:59:47 +0300

    Add files via upload

---
+++ 27. Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+StringBuilder currentString = new StringBuilder();
+currentString.Append(Console.ReadLine());
+
+int power = 0;
+
+for (int i = 0; i < currentString.Length; i++)
+{
+    if (power == 0)
+    {
+        if (currentString[i] == '>')
+        {
+            power += int.Parse(currentString[i + 1].ToString());
+        }
+    }
+    else
+    {
+        if (currentString[i] == '>')
+        {
+            power += int.Parse(currentString[i + 1].ToString());
+        }
+        else
+        {
+            currentString.Remove(i, 1);
+            i--;
+            power--;
+        }
+    }
+}
+
+Console.WriteLine(currentString);
