d7223d0969c35ef5af9ac0693ed86d24cac41cca
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-30 21:57:44 +0300

    Add files via upload

---
+++ 27. Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,11 @@
+﻿char[] text = Console.ReadLine().ToCharArray();
+
+List<char> encryptedText = new List<char>();
+
+for (int i = 0; i < text.Length; i++)
+{
+    int currEncryptedCharValue = text[i] + 3;
+    encryptedText.Add((char)currEncryptedCharValue);
+
+    Console.Write(encryptedText[i]);
+}
