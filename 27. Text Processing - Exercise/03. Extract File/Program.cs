55374139df8a28fd964b7d7378453e8cf3916c07
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-30 21:57:15 +0300

    Add files via upload

---
+++ 27. Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] fileLocation = Console.ReadLine().Split(@"\");
+
+string[] nameAndExtension = fileLocation[fileLocation.Length - 1].Split(".");
+string fileName = nameAndExtension[0];
+string fileExtension = nameAndExtension[1];
+
+Console.WriteLine($"File name: {fileName}");
+Console.WriteLine($"File extension: {fileExtension}");
