67dc8e7c97d5e7306d526091f162e9ca0786b521
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-09-30 21:58:46 +0300

    Add files via upload

---
+++ 27. Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿string input = Console.ReadLine();
+
+List<char> notRepeatedChars = new List<char>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (i == 0)
+    {
+        notRepeatedChars.Add(input[i]);
+    }
+    else
+    {
+        if (input[i] != input[i - 1])
+        {
+            notRepeatedChars.Add(input[i]);
+        }
+    }
+}
+
+foreach (var character in notRepeatedChars)
+{
+    Console.Write(character);
+}
