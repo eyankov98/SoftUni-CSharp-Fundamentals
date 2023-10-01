826279cc56744bd1aad4a2e66e3169dfa1a8d601
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-01 20:13:08 +0300

    Add files via upload

---
+++ 28. Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,27 @@
+﻿char startChar = char.Parse(Console.ReadLine());
+char endChar = char.Parse(Console.ReadLine());
+string inputString = Console.ReadLine();
+
+List<char> chars = new List<char>();
+List<char> outputChars = new List<char>();
+
+for (int i = startChar; i < endChar; i++)
+{
+    char currentChar = (char)i;
+    chars.Add(currentChar);
+}
+
+int sum = 0;
+
+for (int i = 0; i < chars.Count; i++)
+{
+    for (int j = 0; j < inputString.Length; j++)
+    {
+        if (chars[i] == inputString[j])
+        {
+            sum += chars[i];
+        }
+    }
+}
+
+Console.WriteLine(sum);
