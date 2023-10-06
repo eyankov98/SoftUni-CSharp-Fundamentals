7c8d5f92844d06556ab8be57aaffa80ac800707c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-06 20:15:44 +0300

    Add files via upload

---
+++ 33. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+List<string> places = new List<string>();
+
+int travelPoints = 0;
+
+string pattern = @"(?<separator>=|\/)(?<location>[A-Z][A-Za-z]{2,})\1";
+
+Regex regex = new Regex(pattern);
+
+MatchCollection matchCollection = regex.Matches(input);
+
+foreach (Match place in matchCollection)
+{
+    var location = place.Groups["location"].Value;
+    travelPoints = travelPoints + location.Length;
+
+    places.Add(location);
+}
+
+Console.WriteLine($"Destinations: {string.Join(", ", places)}");
+Console.WriteLine($"Travel Points: {travelPoints}");
