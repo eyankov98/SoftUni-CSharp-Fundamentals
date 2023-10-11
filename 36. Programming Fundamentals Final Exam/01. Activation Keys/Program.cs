0d21cf694cc5047185451bc68618f4573d515e92
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-11 20:29:32 +0300

    Add files via upload

---
+++ 36. Programming Fundamentals Final Exam/01. Activation Keys/Program.cs
@@ -0,0 +1,59 @@
+﻿string activationKey = Console.ReadLine();
+
+string input = Console.ReadLine();
+
+while (input != "Generate")
+{
+    string[] command = input
+        .Split(">>>", StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    string currentCommand = command[0];
+
+    if (currentCommand == "Contains")
+    {
+        string substring = command[1];
+
+        if (activationKey.Contains(substring))
+        {
+            Console.WriteLine($"{activationKey} contains {substring}");
+        }
+        else
+        {
+            Console.WriteLine("Substring not found!");
+        }
+    }
+    else if (currentCommand == "Flip")
+    {
+        string upperOrLower = command[1];
+        int startIndex = int.Parse(command[2]);
+        int endIndex = int.Parse(command[3]);
+
+        if (upperOrLower == "Upper")
+        {
+            activationKey = activationKey.Substring(0, startIndex) + activationKey.Substring(startIndex, endIndex - startIndex).ToUpper() + activationKey.Substring(endIndex);
+
+            Console.WriteLine(activationKey);
+        }
+        else if (upperOrLower == "Lower")
+        {
+            activationKey = activationKey.Substring(0, startIndex) + activationKey.Substring(startIndex, endIndex - startIndex).ToLower() + activationKey.Substring(endIndex);
+
+            Console.WriteLine(activationKey);
+        }
+    }
+    else if (currentCommand == "Slice")
+    {
+        int startIndex = int.Parse(command[1]);
+        int endIndex = int.Parse(command[2]);
+
+        //activationKey = activationKey.Substring(0, startIndex) + activationKey.Substring(endIndex);
+        activationKey = activationKey.Remove(startIndex, endIndex - startIndex);
+
+        Console.WriteLine(activationKey);
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"Your activation key is: {activationKey}");
